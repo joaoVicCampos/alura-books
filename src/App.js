b7abcb9e6a42f5c74dc1c3695affe3299521b09b
João Victor Campos <ripjv1234@gmail.com>
2025-03-11 16:24:12 -0300

    ajeita-hedaer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import Logo from './components/Logo'
+import perfil from './imagens/perfil.svg'
+import sacola from './imagens/sacola.svg'
+
+const optList = ['CATEGORIAS', 'FAVORITOS', 'MINHAS ESTANTE']
+const icons = [perfil, sacola]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Logo />
+        <ul className='options'>
+          {optList.map((option) => (
+            <li className='option'><p>{option}</p></li>
+          ))}
+        </ul>
+        <ul className='icons'>
+          {icons.map((icon) => (
+            <li className='icon'><img src={icon} alt='icones'></img></li>
+          ))}
+        </ul>
       </header>
     </div>
   );
